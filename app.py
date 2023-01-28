dbb1cded37923a17dda1583a034ccd536a7ada4e
Justin Sixsmith <95641460+JustinSixsmith@users.noreply.github.com>
2023-01-28 13:50:07 -0600

    uses formatted expressions in a string

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
-students_count = 1000
-rating = 4.9
-print(students_count)
+# students_count = 1000
+# rating = 4.9
+# print(students_count)
 
-course_name = "Python Programming"
-print(len(course_name))
+# course = "Python Programming"
+# print(len(course))
+# print(course[0])
+# print(course[-1])
+# print(course[0:3])
+
+first = "Justin"
+last = "Sixsmith"
+full = f"{first} {last}"
+print(full)
