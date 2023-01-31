f2600b2122d5886b1c0b487ff45515a3dceb4f85
Justin Sixsmith <95641460+JustinSixsmith@users.noreply.github.com>
2023-01-30 23:10:39 -0600

    plays with different string methods

--- app.py
+++ app.py
@@ -8,7 +8,17 @@
 # print(course[-1])
 # print(course[0:3])
 
-first = "Justin"
-last = "Sixsmith"
-full = f"{first} {last}"
-print(full)
+# first = "Justin"
+# last = "Sixsmith"
+# full = f"{first} {last}"
+# print(full)
+
+course = "  python programming"
+print(course.upper())
+print(course.lower())
+print(course.title())
+print(course.strip())
+print(course.find("pro"))
+print(course.replace("p", "j"))
+print("pro" in course)
+print("swift" not in course)
