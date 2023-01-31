c81d0900a6c434bd36a9329748e35672630d6f9f
Justin Sixsmith <95641460+JustinSixsmith@users.noreply.github.com>
2023-01-31 08:12:21 -0600

    works with if and conditional statements

--- app.py
+++ app.py
@@ -49,3 +49,13 @@ import math
 # print(type(x))
 # y = int(x) + 1
 # print(f"x: {x}, y: {y}")
+
+temperature = 21
+if (temperature > 30):
+    print("It's warm")
+    print("Drink water")
+elif (temperature > 20):
+    print("It's nice")
+else:
+    print("It's cold")
+print("Done")
