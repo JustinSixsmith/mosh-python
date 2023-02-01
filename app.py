c8d28a2b374c3ea211de55e1f3b7dd169900a2b5
Justin Sixsmith <95641460+JustinSixsmith@users.noreply.github.com>
2023-01-31 18:06:31 -0600

    short-circuit evaluation

--- app.py
+++ app.py
@@ -70,11 +70,9 @@ import math
 # message = "Eligible" if age >= 18 else "Not eligible"
 # print(message)
 
-high_income = True
+high_income = False
 good_credit = True
 student = False
 
-if (high_income or good_credit) and not student:
+if high_income or good_credit or not student:
     print("Eligible")
-else:
-    print("Not eligible")
