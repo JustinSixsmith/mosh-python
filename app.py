c5af53e12cf659ad99cbaa40a0b4835773e644a0
Justin Sixsmith <95641460+JustinSixsmith@users.noreply.github.com>
2023-01-31 18:57:11 -0600

    answers quiz using a for loop and a formatted string

--- app.py
+++ app.py
@@ -115,8 +115,15 @@ import math
 #     print(number)
 #     number //= 2
 
-while True:
-    command = input(">")
-    print("ECHO", command)
-    if command.lower() == "quit":
-        break
+# while True:
+#     command = input(">")
+#     print("ECHO", command)
+#     if command.lower() == "quit":
+#         break
+
+count = 0
+for number in range(1, 10 + 1):
+    if number % 2 == 0:
+        print(number)
+        count += 1
+print(f"We have {count} even numbers")
