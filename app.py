147989f7159c73290b2df9069c3b4d8881967de5
Justin Sixsmith <95641460+JustinSixsmith@users.noreply.github.com>
2023-01-31 18:35:44 -0600

    creates a nested loop

--- app.py
+++ app.py
@@ -85,11 +85,15 @@ import math
 # for number in range(1, 10, 2):
 #     print("Attempt", number, (number + 1) * ".")
 
-successful = True
-for number in range(3):
-    print("Attempt")
-    if successful:
-        print("Successful")
-        break
-else:
-    print("Attempted 3 times and failed")
+# successful = True
+# for number in range(3):
+#     print("Attempt")
+#     if successful:
+#         print("Successful")
+#         break
+# else:
+#     print("Attempted 3 times and failed")
+
+for x in range(5):
+    for y in range(3):
+        print(f"({x}, {y})")
