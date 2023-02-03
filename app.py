748753e6c70ae747a56593df164b586e0632bbdd
Justin Sixsmith <95641460+JustinSixsmith@users.noreply.github.com>
2023-02-03 10:13:32 -0600

    accesses items in list

--- app.py
+++ app.py
@@ -1,12 +1,23 @@
 import math
 
-letters = ["a", "b", "c"]
-matrix = [[0, 1], [2, 3]]
-zeros = [0] * 5
-combined = zeros + letters
 numbers = list(range(20))
-chars = list("Hello World")
-print(len(chars))
+print(numbers[::2])
+print(numbers[::-1])
+
+
+# letters = ["a", "b", "c", "d"]
+# letters[0] = "A"
+# print(letters[0:3])
+# print(letters[::2])
+
+
+# letters = ["a", "b", "c"]
+# matrix = [[0, 1], [2, 3]]
+# zeros = [0] * 5
+# combined = zeros + letters
+# numbers = list(range(20))
+# chars = list("Hello World")
+# print(len(chars))
 
 
 # def fizz_buzz(input):
