1975ca82638ab882383f7eac2453fa626b8daaf5
Justin Sixsmith <95641460+JustinSixsmith@users.noreply.github.com>
2023-02-03 17:41:21 -0600

    works with zip functions

--- app.py
+++ app.py
@@ -1,18 +1,26 @@
 import math
 
+list1 = [1, 2, 3]
+list2 = [10, 20, 30]
 
-items = [
-    ("Product1", 10),
-    ("Product2", 9),
-    ("Product3", 12)
-]
+print(list(zip("abc", list1, list2)))
 
-filtered = list(filter(lambda item: item[1] >= 10, items))
-print(filtered)
+
+# items = [
+#     ("Product1", 10),
+#     ("Product2", 9),
+#     ("Product3", 12)
+# ]
 
 
 # prices = list(map(lambda item: item[1], items))
 # print(prices)
+# prices = [item[1] for item in items]
+
+
+# filtered = list(filter(lambda item: item[1] >= 10, items))
+# print(filtered)
+# filtered = [for item in items if item[1] >= 10 ]
 
 
 # items.sort(key=lambda item: item[1])
