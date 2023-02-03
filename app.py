c9c73133e8ad32cad6c39bd3b952d76890b262c7
Justin Sixsmith <95641460+JustinSixsmith@users.noreply.github.com>
2023-02-03 10:08:40 -0600

    works with creating lists

--- app.py
+++ app.py
@@ -1,18 +1,25 @@
 import math
 
-
-def fizz_buzz(input):
-    if input % 3 == 0 and input % 5 == 0:
-        return "FizzBuzz"
-    elif input % 3 == 0:
-        return "Fizz"
-    elif input % 5 == 0:
-        return "Buzz"
-    else:
-        return input
+letters = ["a", "b", "c"]
+matrix = [[0, 1], [2, 3]]
+zeros = [0] * 5
+combined = zeros + letters
+numbers = list(range(20))
+chars = list("Hello World")
+print(len(chars))
 
 
-print(fizz_buzz(7))
+# def fizz_buzz(input):
+#     if (input % 3 == 0) and (input % 5 == 0):
+#         return "FizzBuzz"
+#     if input % 3 == 0:
+#         return "Fizz"
+#     if input % 5 == 0:
+#         return "Buzz"
+#     return input
+
+
+# print(fizz_buzz(7))
 
 
 # def greet():
