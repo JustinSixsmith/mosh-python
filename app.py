dcaae7420b8edfc0d714585d8633027edf9f241f
Justin Sixsmith <95641460+JustinSixsmith@users.noreply.github.com>
2023-02-06 10:53:43 -0600

    checks operation time comparing pass exception handling using timeit

--- app.py
+++ app.py
@@ -3,18 +3,34 @@
 # from array import array
 # from sys import getsizeof
 # from pprint import pprint
+from timeit import timeit
 
-
+code1 = """
 def calculate_xfactor(age):
     if age <= 0:
         raise ValueError("Age cannot be zero or less.")
     return 10 / age
 
-
 try:
     calculate_xfactor(-1)
 except ValueError as error:
-    print(error)
+    pass
+"""
+
+
+code2 = """
+def calculate_xfactor(age):
+    if age <= 0:
+        return None
+    return 10 / age
+
+xfactor = calculate_xfactor(-1)
+# if xfactor == None:
+#     pass
+"""
+
+print("first code=", timeit(code1, number=10000))
+print("second code=", timeit(code2, number=10000))
 
 
 # try:
