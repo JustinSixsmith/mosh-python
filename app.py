4374bece8783a5a0f9398d8f36881450b4363579
Justin Sixsmith <95641460+JustinSixsmith@users.noreply.github.com>
2023-02-06 10:01:03 -0600

    adds a ZeroDivisionError clause

--- app.py
+++ app.py
@@ -7,8 +7,12 @@
 
 try:
     age = int(input("Age: "))
-except ValueError:
+    xfactor = 10 / age
+except (ValueError, ZeroDivisionError):
     print("You didn't enter a valid age.")
+else:
+    print("No exceptions were thrown.")
+print("Excecution continues.")
 
 
 # sentence = "This is a common interview question"
