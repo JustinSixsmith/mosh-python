ea5fc363bf2b164d83e4a98777150f0004c8e66a
Justin Sixsmith <95641460+JustinSixsmith@users.noreply.github.com>
2023-02-07 09:31:25 -0600

    defines get/set items, length and iterates over custom container

--- .vscode/class-tutorial.py
+++ .vscode/class-tutorial.py
@@ -1,34 +1,49 @@
-class Point:
-    default_color = "red"
+class TagCloud:
+    def __init__(self):
+        self.tags = {}
 
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-
-    def __str__(self):
-        return f"({self.x}, {self.y})"
-
-    def __eq__(self, other):
-        return self.x == other.x and self.y == other.y
-
-    def __gt__(self, other):
-        return self.x > other.x and self.y > other.y
-
-    def __add__(self, other):
-        return Point(self.x + other.x, self.y + other.y)
-
-    @classmethod
-    def zero(cls):
-        return cls(0, 0)
-
-    def draw(self):
-        print(f"Point ({self.x}, {self.y})")
+    def add(self, tag):
+        self.tags[tag.lower()] = self.tags.get(tag.lower(), 0) + 1
 
 
-point1 = Point(10, 20)
-point2 = Point(1, 2)
-combined = point1 + point2
-print(combined.x)
+cloud = TagCloud()
+cloud.add("Python")
+cloud.add("python")
+cloud.add("python")
+print(cloud.tags)
+
+
+# class Point:
+#     default_color = "red"
+
+#     def __init__(self, x, y):
+#         self.x = x
+#         self.y = y
+
+#     def __str__(self):
+#         return f"({self.x}, {self.y})"
+
+#     def __eq__(self, other):
+#         return self.x == other.x and self.y == other.y
+
+#     def __gt__(self, other):
+#         return self.x > other.x and self.y > other.y
+
+#     def __add__(self, other):
+#         return Point(self.x + other.x, self.y + other.y)
+
+#     @classmethod
+#     def zero(cls):
+#         return cls(0, 0)
+
+#     def draw(self):
+#         print(f"Point ({self.x}, {self.y})")
+
+
+# point1 = Point(10, 20)
+# point2 = Point(1, 2)
+# combined = point1 + point2
+# print(combined.x)
 
 # Point.default_color = "yellow"
 # point = Point(1, 2)
