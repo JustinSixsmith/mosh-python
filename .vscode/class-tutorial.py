fc223d8225b65f7d6d648b7a3d9b59da5e65827e
Justin Sixsmith <95641460+JustinSixsmith@users.noreply.github.com>
2023-02-07 09:07:09 -0600

    defines greater than magic method

--- .vscode/class-tutorial.py
+++ .vscode/class-tutorial.py
@@ -8,6 +8,12 @@ class Point:
     def __str__(self):
         return f"({self.x}, {self.y})"
 
+    def __eq__(self, other):
+        return self.x == other.x and self.y == other.y
+
+    def __gt__(self, other):
+        return self.x > other.x and self.y > other.y
+
     @classmethod
     def zero(cls):
         return cls(0, 0)
@@ -16,9 +22,9 @@ class Point:
         print(f"Point ({self.x}, {self.y})")
 
 
-point = Point.zero()
-point.draw()
-print(str(point))
+point1 = Point(10, 20)
+point2 = Point(1, 2)
+print(point1 < point2)
 
 # Point.default_color = "yellow"
 # point = Point(1, 2)
