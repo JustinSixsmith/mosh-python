05d47a618d298391dee01a48ea9a59ece0b6dfca
Justin Sixsmith <95641460+JustinSixsmith@users.noreply.github.com>
2023-02-07 09:10:40 -0600

    combines objects by defining add magic method

--- .vscode/class-tutorial.py
+++ .vscode/class-tutorial.py
@@ -14,6 +14,9 @@ class Point:
     def __gt__(self, other):
         return self.x > other.x and self.y > other.y
 
+    def __add__(self, other):
+        return Point(self.x + other.x, self.y + other.y)
+
     @classmethod
     def zero(cls):
         return cls(0, 0)
@@ -24,7 +27,8 @@ class Point:
 
 point1 = Point(10, 20)
 point2 = Point(1, 2)
-print(point1 < point2)
+combined = point1 + point2
+print(combined.x)
 
 # Point.default_color = "yellow"
 # point = Point(1, 2)
