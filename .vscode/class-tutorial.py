bb8d0b34a125a9c43ba5a56f0f0705295d3be8f4
Justin Sixsmith <95641460+JustinSixsmith@users.noreply.github.com>
2023-02-08 13:32:08 -0600

    defines Point object as a namedtuple

--- .vscode/class-tutorial.py
+++ .vscode/class-tutorial.py
@@ -1,26 +1,42 @@
 from abc import ABC, abstractclassmethod
+from collections import namedtuple
 
 
 class InvalidOperationError(Exception):
     pass
 
 
-class Text(str):
-    def duplicate(self):
-        return self + self
+# class Point:
+#     def __init__(self, x, y):
+#         self.x = x
+#         self.y = y
+
+#     def __eq__(self, other):
+#         return self.x == other.x and self.y == other.y
 
 
-class TrackableList(list):
-    def append(self, object):
-        print("Append called")
-        super().append(object)
+Point = namedtuple("Point", ["x", "y"])
+p1 = Point(x=1, y=2)
+p2 = Point(x=1, y=2)
+print(p1 == p2)
 
 
-text = Text("Python")
-print(text.duplicate())
+# class Text(str):
+#     def duplicate(self):
+#         return self + self
 
-list = TrackableList()
-list.append("1")
+
+# class TrackableList(list):
+#     def append(self, object):
+#         print("Append called")
+#         super().append(object)
+
+
+# text = Text("Python")
+# print(text.duplicate())
+
+# list = TrackableList()
+# list.append("1")
 
 
 # class UIControl(ABC):
