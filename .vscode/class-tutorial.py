e2afb0cb50aabd0bc4c9131687a99185a5a6b652
Justin Sixsmith <95641460+JustinSixsmith@users.noreply.github.com>
2023-02-08 12:06:35 -0600

    overrides Animal constructor

--- .vscode/class-tutorial.py
+++ .vscode/class-tutorial.py
@@ -1,21 +1,53 @@
-class Product:
-    def __init__(self, price):
-        self.price = price
 
-    @property
-    def price(self):
-        return self.__price
+class Animal:
+    def __init__(self):
+        self.age = 1
 
-    @price.setter
-    def price(self, value):
-        if value < 0:
-            raise ValueError("Price cannot be negative.")
-        self.__price = value
+    def eat(self):
+        print("eat")
 
 
-product = Product(10)
-product.price = -1
-print(product.price)
+class Mammal(Animal):
+    def __init__(self):
+        super().__init__()
+        self.weight = 2
+
+    def walk(self):
+        print("walk")
+
+
+class Fish(Animal):
+    def swim(self):
+        print("swim")
+
+
+class Bird(Animal):
+    def fly(self):
+        print("fly")
+
+
+m = Mammal()
+print(m.age)
+print(m.weight)
+
+
+# class Product:
+#     def __init__(self, price):
+#         self.price = price
+
+#     @property
+#     def price(self):
+#         return self.__price
+
+#     @price.setter
+#     def price(self, value):
+#         if value < 0:
+#             raise ValueError("Price cannot be negative.")
+#         self.__price = value
+
+
+# product = Product(10)
+# print(product.price)
 
 # class TagCloud:
 #     def __init__(self):
