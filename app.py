3a70f2799e895aea1fb2bda44720fe7bbc70070c
Justin Sixsmith <95641460+JustinSixsmith@users.noreply.github.com>
2023-02-11 21:32:02 -0600

    get py files in ecommerce directory and its children

--- app.py
+++ app.py
@@ -2,5 +2,6 @@ from pathlib import Path
 
 path = Path("ecommerce")
 
-paths = [p for p in path.iterdir()]
-print(paths)
+paths = [p for p in path.iterdir() if p.is_dir()]
+py_files = [p for p in path.rglob("*.py")]
+print(py_files)
