842953abea22c2871dd03b6086fb7a8f0989a076
Justin Sixsmith <95641460+JustinSixsmith@users.noreply.github.com>
2023-02-11 21:38:08 -0600

    prints time __init__.py file was created :

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from pathlib import Path
+from time import ctime
 
-path = Path("ecommerce")
+path = Path("ecommerce/__init__.py")
+print(ctime(path.stat().st_ctime))
 
-paths = [p for p in path.iterdir() if p.is_dir()]
-py_files = [p for p in path.rglob("*.py")]
-print(py_files)
+
+# paths = [p for p in path.iterdir() if p.is_dir()]
+# py_files = [p for p in path.rglob("*.py")]
+# print(py_files)
