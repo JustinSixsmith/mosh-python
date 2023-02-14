ad216924a9bc7bdbf6defac7a886eda3653ea5c8
Justin Sixsmith <95641460+JustinSixsmith@users.noreply.github.com>
2023-02-14 09:20:23 -0600

    explores file and zip functions and methods

--- app.py
+++ app.py
@@ -1,8 +1,25 @@
+# from time import ctime
+# import shutil
 from pathlib import Path
-from time import ctime
+from zipfile import ZipFile
 
-path = Path("ecommerce/__init__.py")
-print(ctime(path.stat().st_ctime))
+# with ZipFile("files.zip", "w") as zip:
+#     for path in Path("ecommerce").rglob("*.*"):
+#         zip.write(path)
+
+with ZipFile("files.zip") as zip:
+    info = zip.getinfo("ecommerce/__init__.py")
+    print(info.file_size)
+    print(info.compress_size)
+    zip.extractall("extract")
+
+# source = Path("ecommerce/__init__.py")
+# target = Path() / "__init__.py"
+# shutil.copy(source, target)
+
+
+# print(ctime(path.stat().st_ctime))
+# print(path.read_text())
 
 
 # paths = [p for p in path.iterdir() if p.is_dir()]
