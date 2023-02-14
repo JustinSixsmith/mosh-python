41a06a1d86440c1217e12c6d330d07fd36090607
Justin Sixsmith <95641460+JustinSixsmith@users.noreply.github.com>
2023-02-14 09:36:33 -0600

    writes and reads rows in data csv file

--- app.py
+++ app.py
@@ -4,11 +4,11 @@
 # from zipfile import ZipFile
 import csv
 
-with open("data.csv", "w") as file:
-    writer = csv.writer(file)
-    writer.writerow(["trancaction_id", "product_id", "price"])
-    writer.writerow([1000, 1, 5])
-    writer.writerow([1001, 2, 15])
+with open("data.csv") as file:
+    reader = csv.reader(file)
+    # print(list(reader))
+    for row in reader:
+        print(row)
 
 
 # with ZipFile("files.zip", "w") as zip:
