b440cbb5e47a4f0919d61947bdb5c82d4bb9c726
Justin Sixsmith <95641460+JustinSixsmith@users.noreply.github.com>
2023-02-14 09:32:43 -0600

    creates a simple csv file

--- app.py
+++ app.py
@@ -1,17 +1,25 @@
 # from time import ctime
 # import shutil
-from pathlib import Path
-from zipfile import ZipFile
+# from pathlib import Path
+# from zipfile import ZipFile
+import csv
+
+with open("data.csv", "w") as file:
+    writer = csv.writer(file)
+    writer.writerow(["trancaction_id", "product_id", "price"])
+    writer.writerow([1000, 1, 5])
+    writer.writerow([1001, 2, 15])
+
 
 # with ZipFile("files.zip", "w") as zip:
 #     for path in Path("ecommerce").rglob("*.*"):
 #         zip.write(path)
 
-with ZipFile("files.zip") as zip:
-    info = zip.getinfo("ecommerce/__init__.py")
-    print(info.file_size)
-    print(info.compress_size)
-    zip.extractall("extract")
+# with ZipFile("files.zip") as zip:
+#     info = zip.getinfo("ecommerce/__init__.py")
+#     print(info.file_size)
+#     print(info.compress_size)
+#     zip.extractall("extract")
 
 # source = Path("ecommerce/__init__.py")
 # target = Path() / "__init__.py"
